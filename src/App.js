2f3f7909b28a1021de4b20c9e2677afb22ac5cad
SaraMoreiraG <sara_moreira@hotmail.es>
2024-04-17 15:39:33 +0200

    adding banner

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
         <div className='row m-0'>
-        <img src={banner} className='img-fluid p-0'/>
+        <img src={banner} className='img-fluid p-0' alt='banner parking las praderas'/>
         </div>
     </div>
   );
